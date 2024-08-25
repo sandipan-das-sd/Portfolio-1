29204277f436a8d042e264950df8f124a59e21a0
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-08-25 22:11:11 +0530

    changes

--- client/src/components/About/AboutCard.js
+++ client/src/components/About/AboutCard.js
@@ -31,7 +31,7 @@ function AboutCard() {
             </li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
+          <p style={{ color: "#00ff99" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
           <footer className="blockquote-footer">Soumyajit</footer>
